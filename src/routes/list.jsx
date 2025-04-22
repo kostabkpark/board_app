cf82bc9459889a20722aea38874d952c433dabdc
kostabkpark <kostabkpark@gmail.com>
2025-04-22 10:03:32 +0900

    list.jsx -> Table

--- src/routes/list.jsx
+++ src/routes/list.jsx
@@ -1,22 +1,49 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import MyTable from "../MyTable"
+//import MyTable from "../MyTable"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import  Table  from 'react-bootstrap/Table'
 
 function List() {
     const [list, setList] = useState([]);
     useEffect(()=>{
-        const list_db = axios.get("http://localhost:5000/api/boards")
-                            .then((res)=> {
-                                console.log(res.data);
-                            })
-                            .catch((err)=>{
-                                console.log(err);
-                            });
-        }, []);
+        axios.get("http://localhost:5000/api/boards")
+            .then((res)=> {
+                console.log("res.data", res.data);
+                setList(res.data);
+            })
+            .catch((err)=>{
+                console.log(err);
+            });         
+    }, []);
+    console.log("list", list);
     return (
         <>
             <h2>List.jsx</h2>
-            <MyTable />
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>작성일자</th>
+                        <th>조회수</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {list.map((board)=>{
+                        return (
+                        <tr key={board.id}>
+                            <td>{board.id}</td>
+                            <td>{board.title}</td>
+                            <td>{board.writer}</td>
+                            <td>{board.createdAt}</td>
+                            <td>{board.hits}</td>
+                        </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
         </>
     )
 }
